b7dbf92c0e1695931e4c2ce1cd61161f771e1c1a
xiayx <xiayouxue@gold32.com>
2020-03-11 07:30:40 +0800

    fix javadoc =>  to =&gt;

--- peacetrue-sql-metadata/src/main/java/com/github/peacetrue/sql/metadata/ColumnDescriptionUtils.java
+++ peacetrue-sql-metadata/src/main/java/com/github/peacetrue/sql/metadata/ColumnDescriptionUtils.java
@@ -5,48 +5,48 @@ package com.github.peacetrue.sql.metadata;
  */
 public class ColumnDescriptionUtils {
 
-    /** 1.String => table catalog (may be null) */
+    /** 1.String =&gt; table catalog (may be null) */
     public static final String TABLE_CAT = "TABLE_CAT";
-    /** 2.String => table schema (may be null) */
+    /** 2.String =&gt; table schema (may be null) */
     public static final String TABLE_SCHEM = "TABLE_SCHEM";
-    /** 3.String => table name */
+    /** 3.String =&gt; table name */
     public static final String TABLE_NAME = "TABLE_NAME";
-    /** 4.String => column name */
+    /** 4.String =&gt; column name */
     public static final String COLUMN_NAME = "COLUMN_NAME";
-    /** 5.int => SQL type from java.sql.Types */
+    /** 5.int =&gt; SQL type from java.sql.Types */
     public static final String DATA_TYPE = "DATA_TYPE";
-    /** 6.String => Data source dependent type name, for a UDT the type name is fully qualified */
+    /** 6.String =&gt; Data source dependent type name, for a UDT the type name is fully qualified */
     public static final String TYPE_NAME = "TYPE_NAME";
-    /** 7.int => column size */
+    /** 7.int =&gt; column size */
     public static final String COLUMN_SIZE = "COLUMN_SIZE";
     /** 8.is not used */
     public static final String BUFFER_LENGTH = "BUFFER_LENGTH";
-    /** 9.int => the number of fractional digits. Null is returned for data types where DECIMAL_DIGITS is not applicable. */
+    /** 9.int =&gt; the number of fractional digits. Null is returned for data types where DECIMAL_DIGITS is not applicable. */
     public static final String DECIMAL_DIGITS = "DECIMAL_DIGITS";
-    /** 10.int => Radix (typically either 10 or 2) */
+    /** 10.int =&gt; Radix (typically either 10 or 2) */
     public static final String NUM_PREC_RADIX = "NUM_PREC_RADIX";
     /**
-     * 11.int => is NULL allowed.
+     * 11.int =&gt; is NULL allowed.
      *
      * @see java.sql.DatabaseMetaData#columnNullable
      * @see java.sql.DatabaseMetaData#columnNoNulls
      * @see java.sql.DatabaseMetaData#columnNullableUnknown
      */
     public static final String NULLABLE = "NULLABLE";
-    /** 12.String => comment describing column (may be null) */
+    /** 12.String =&gt; comment describing column (may be null) */
     public static final String REMARKS = "REMARKS";
-    /** 13.String => default value for the column, which should be interpreted as a string when the value is enclosed in single quotes (may be null) */
+    /** 13.String =&gt; default value for the column, which should be interpreted as a string when the value is enclosed in single quotes (may be null) */
     public static final String COLUMN_DEF = "COLUMN_DEF";
-    /** 14.int => unused */
+    /** 14.int =&gt; unused */
     public static final String SQL_DATA_TYPE = "SQL_DATA_TYPE";
-    /** 15.int => unused */
+    /** 15.int =&gt; unused */
     public static final String SQL_DATETIME_SUB = "SQL_DATETIME_SUB";
-    /** 16.int => for char types the maximum number of bytes in the column */
+    /** 16.int =&gt; for char types the maximum number of bytes in the column */
     public static final String CHAR_OCTET_LENGTH = "CHAR_OCTET_LENGTH";
-    /** 17.int => index of column in table (starting at 1) */
+    /** 17.int =&gt; index of column in table (starting at 1) */
     public static final String ORDINAL_POSITION = "ORDINAL_POSITION";
     /**
-     * 18.String => ISO rules are used to determine the nullability for a column.
+     * 18.String =&gt; ISO rules are used to determine the nullability for a column.
      * <pre>
      * YES --- if the column can include NULLs
      * NO --- if the column cannot include NULLs
@@ -54,18 +54,18 @@ public class ColumnDescriptionUtils {
      * </pre>
      */
     public static final String IS_NULLABLE = "IS_NULLABLE";
-    /** 19.String => catalog of table that is the scope of a reference attribute (null if DATA_TYPE isn't REF) */
+    /** 19.String =&gt; catalog of table that is the scope of a reference attribute (null if DATA_TYPE isn't REF) */
     public static final String SCOPE_CATALOG = "SCOPE_CATALOG";
-    /** 20.String => schema of table that is the scope of a reference attribute (null if the DATA_TYPE isn't REF) */
+    /** 20.String =&gt; schema of table that is the scope of a reference attribute (null if the DATA_TYPE isn't REF) */
     public static final String SCOPE_SCHEMA = "SCOPE_SCHEMA";
-    /** 21.String => table name that this the scope of a reference attribute (null if the DATA_TYPE isn't REF) */
+    /** 21.String =&gt; table name that this the scope of a reference attribute (null if the DATA_TYPE isn't REF) */
     public static final String SCOPE_TABLE = "SCOPE_TABLE";
-    /** 22.short => source type of a distinct type or user-generated Ref type, SQL type from java.sql.Types (null if DATA_TYPE isn't DISTINCT or user-generated REF) */
+    /** 22.short =&gt; source type of a distinct type or user-generated Ref type, SQL type from java.sql.Types (null if DATA_TYPE isn't DISTINCT or user-generated REF) */
     public static final String SOURCE_DATA_TYPE = "SOURCE_DATA_TYPE";
     /**
-     * 23.String => Indicates whether this column is auto incremented
+     * 23.String =&gt; Indicates whether this column is auto incremented
      * <pre>
-     * IS_AUTOINCREMENT String => Indicates whether this column is auto incremented
+     * IS_AUTOINCREMENT String =&gt; Indicates whether this column is auto incremented
      * YES --- if the column is auto incremented
      * NO --- if the column is not auto incremented
      * empty string --- if it cannot be determined whether the column is auto incremented
@@ -73,7 +73,7 @@ public class ColumnDescriptionUtils {
      */
     public static final String IS_AUTOINCREMENT = "IS_AUTOINCREMENT";
     /**
-     * 24.String => Indicates whether this is a generated column
+     * 24.String =&gt; Indicates whether this is a generated column
      * <pre>
      * YES --- if this a generated column
      * NO --- if this not a generated column
